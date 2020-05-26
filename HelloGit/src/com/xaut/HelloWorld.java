202a0bfb12a0b996a0d2725947cf2a098bdf832f
Your NameWhb133 <1336547912@qq.com>
2020-05-26 22:33:28 +0800

    添加文件

---
+++ HelloGit/src/com/xaut/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.xaut;
+
+import java.sql.SQLOutput;
+
+/**
+ * @author whb
+ * @date 2020/5/26-22:26
+ * @
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
---
+++ HelloGit/src/com/xaut/Stu.java
@@ -0,0 +1,9 @@
+package com.xaut;
+
+/**
+ * @author whb
+ * @date 2020/5/26-22:31
+ * @
+ */
+public class Stu {
+}

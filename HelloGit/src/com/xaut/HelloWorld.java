f427042c2dd85aa12f4ff42421bdb04ed4f5185b
Your NameWhb133 <1336547912@qq.com>
2020-05-27 09:48:37 +0800

    更新1

--- HelloGit/src/com/xaut/HelloWorld.java
+++ HelloGit/src/com/xaut/HelloWorld.java
@@ -10,5 +10,6 @@ import java.sql.SQLOutput;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("helloworld");
+        System.out.println("更新1");
     }
 }
